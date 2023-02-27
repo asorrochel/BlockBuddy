e032e18456e84dad302507359f83147348b7bba5
Adrián Sorroche <asorrocheL@protonmail.com>
2023-02-27 17:04:15 +0100

    imp main menu Usuario

--- app/src/main/java/com/example/blockbuddytfg/MainActivity.java
+++ app/src/main/java/com/example/blockbuddytfg/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.blockbuddytfg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TextView mainUsuario_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        inicializarHooks();
+
+        mainUsuario_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AjustesPerfil.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * Método que cierra el teclado cuando se pulsa fuera de un EditText.
+     * @param event - Evento que se produce al pulsar fuera de un EditText.
+     * @return - True, si se ha pulsado fuera de un EditText, False si no se ha pulsado.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Guardamos la vista seleccionada.
+        View view = this.getCurrentFocus();
+
+        // Si no es null (Tenemos una vista seleccionada), cerramos el teclado.
+        if (view != null) {
+            Context context = getApplicationContext();
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Método para inicializar los hooks.
+    private void inicializarHooks() {
+        mainUsuario_textView = findViewById(R.id.mainUsuario_textView);
     }
 }
